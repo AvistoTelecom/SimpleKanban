1db2f0029a9bfccb1c6cfa8e5fd6916387e1e5b7
Marie-Almina Gindre <UF556MGI@net.elsys-design.com>
2024-03-08 16:12:48 +0100

    Added type to component NavBar

--- src/NavBar.tsx
+++ src/NavBar.tsx
@@ -1,4 +1,4 @@
-export const NavBar = () => {
+export const NavBar = (): React.JSX.Element => {
   return (
     <nav className="navbar bg-base-100">
       <button className="btn btn-ghost text-xl">Button</button>
