c45ab579e92928387eca0c2ea1b5de857f8cba35
Tayeb Hakkar <UF303THA@net.elsys-design.com>
2024-03-15 14:52:26 +0100

    Basic button

--- src/components/TicketColumn.tsx
+++ src/components/TicketColumn.tsx
@@ -1,9 +1,11 @@
 import { FunctionComponent } from 'react';
+import { NewTicketButton } from './NewTicketButton';
 
 export const TicketConumn: FunctionComponent = () => {
   return (
-    <div className="flex-1 bg-base-200 place-items-center min-w-60 h-full rounded-box">
+    <div className="flex-1 grid grid-rows-[95%_5%] bg-base-200 place-items-center min-w-60 h-full rounded-box">
       Ticket column
+      <NewTicketButton onClick={() => console.log('hello')} />
     </div>
   );
 };
