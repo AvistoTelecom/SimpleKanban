d4f23f1076db1d4ce9c5fca6549a95ac76c327a9
Benjamin Mitton <benjamin.mitton@avisto.com>
2024-03-15 15:41:04 +0000

    Removing blank spaces

--- src/components/KanbanArea.tsx
+++ src/components/KanbanArea.tsx
@@ -3,7 +3,7 @@ import { TicketColumn } from './TicketColumn';
 
 export const KanbanArea: FunctionComponent = () => {
   return (
-    <div className="flex bg-base-100 rounded-box w-full gap-2 p-2 overflow-x-auto	">
+    <div className="flex bg-base-100 rounded-box w-full gap-2 p-2 overflow-x-auto">
       <TicketColumn />
       <TicketColumn />
       <TicketColumn />
