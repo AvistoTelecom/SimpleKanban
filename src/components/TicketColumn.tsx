77e0b21ff5a8aac6d024c768163f4ba83220f51d
Tayeb Hakkar <UF303THA@net.elsys-design.com>
2024-03-18 13:38:23 +0100

    Update imports & removing console log

--- src/components/TicketColumn.tsx
+++ src/components/TicketColumn.tsx
@@ -1,11 +1,11 @@
 import { FunctionComponent } from 'react';
-import { NewTicketButton } from './AddTicketButton';
+import { AddTicketButton } from './AddTicketButton';
 
 export const TicketConumn: FunctionComponent = () => {
   return (
     <div className="flex-1 grid grid-rows-[95%_5%] bg-base-200 place-items-center min-w-60 h-full rounded-box">
       Ticket column
-      <NewTicketButton onClick={() => console.log('hello')} />
+      <AddTicketButton onClick={() => {}} />
     </div>
   );
 };
