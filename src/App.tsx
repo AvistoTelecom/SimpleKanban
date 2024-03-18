491621453b5d17a4a0316d4217397ddb0557dd0f
Tayeb Hakkar <UF303THA@net.elsys-design.com>
2024-03-18 16:02:06 +0100

    Adding provider to the app

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,20 @@ import { KanbanPage } from './components/KanbanPage';
 import { FunctionComponent } from 'react';
 import UsersContextProvider from './components/context/UsersContext';
 import TagsContextProvider from './components/context/TagsContext';
+import { TicketContextProvider } from './components/context/TicketContext';
 
 export const App: FunctionComponent = () => {
   return (
     <UsersContextProvider>
       <TagsContextProvider>
-        <div className="h-screen flex flex-col">
-          <Routes>
-            <Route path="/" element={<KanbanPage />} />
-            <Route path="*" element={<Navigate to="/" />} />
-          </Routes>
-        </div>
+        <TicketContextProvider>
+          <div className="h-screen flex flex-col">
+            <Routes>
+              <Route path="/" element={<KanbanPage />} />
+              <Route path="*" element={<Navigate to="/" />} />
+            </Routes>
+          </div>
+        </TicketContextProvider>
       </TagsContextProvider>
     </UsersContextProvider>
   );
